fbb9fbd6e4385ab7dfebad92555942b031e3a901
Rellyson Douglas <rellysondouglas2015@gmail.com>
2022-10-13 11:09:12 -0300

    design system of Ignite Lab

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './styles/global.css'
+
+export function App() {
+  return (
+    <>
+      <h1 className="font-bold text-2xl text-violet-700">Hello world</h1>
+
+      <button className="bg-cyan-600 font-medium px-4 py-2 rounded text-white hover:bg-cyan-300">
+        Enviar
+      </button>
+    </>
+  )
+}

07363f083f96616c49125a16680f8b3898248f3e
Rellyson Douglas <rellysondouglas2015@gmail.com>
2022-10-15 11:30:59 -0300

    Changing and using components to create login page

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -6,20 +6,22 @@ export interface TextProps {
   size?: 'sm' | 'md' | 'lg',
   children: ReactNode,
   asChild?: boolean,
+  className?: string
 }
 
-export function Text({ size = 'md', children, asChild }: TextProps) {
+export function Text({ size = 'md', children, asChild, className }: TextProps) {
   const Comp = asChild ? Slot : 'span'
 
   return (
     <Comp
       className={clsx(
-        'text-gray-200 font-sans text-lg',
+        'text-gray-100 font-sans text-lg',
         {
           'text-xs': size === 'sm',
           'text-sm': size === 'md',
           'text-md': size === 'lg',
-        }
+        },
+        className
       )}
     >
       {children}
